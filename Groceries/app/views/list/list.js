e55d2bd8d0cd877313bbad518f7dc2e8e269469c
Martin Pettersson <kontakt@martinpettersson.se>
2019-06-25 08:17:27 +0200

    Grocery list array functions

--- Groceries/app/views/list/list.js
+++ Groceries/app/views/list/list.js
@@ -1,14 +1,13 @@
 var dialogsModule = require("ui/dialogs");
 var observableModule = require("data/observable");
 var ObservableArray = require("data/observable-array").ObservableArray;
+var GroceryListViewModel = require("../../shared/view-models/grocery-list-view-model");
+
 var page;
 
-var pageData = new observableModule.fromObject({
-    groceryList: new ObservableArray([
-        { name: "eggs" },
-        { name: "bread" },
-        { name: "cereal" }
-    ])
+var groceryList = new GroceryListViewModel([]);
+var pageData = observableModule.fromObject({
+    groceryList: groceryList
 });
 
 exports.loaded = function (args) {
--- Groceries/platforms/android/app/src/main/assets/app/shared/view-models/grocery-list-view-model.js
+++ Groceries/platforms/android/app/src/main/assets/app/shared/view-models/grocery-list-view-model.js
@@ -6,7 +6,29 @@ function GroceryListViewModel(items) {
     var baseUrl = config.apiUrl + "appdata/" + config.appKey + "/Groceries";
     var viewModel = new ObservableArray(items);
 
+    viewModel.load = function() {
+        return fetchModule.fetch(baseUrl, {
+            headers: getCommonHeaders()
+        })
+            .then(handleErrors)
+            .then(function (response) {
+                return response.json();
+            })  
+            .then(function (data) {
+                data.forEach((grocery) => {
+                    viewModel.push({
+                        id: grocery._id,
+                        name: grocery.Name
+                    });
+                });
+            });
+    };
 
+    viewModel.empty = function () {
+        while (viewModel.length) {
+            viewModel.pop();
+        }
+    };
 
     return viewModel;
 }
