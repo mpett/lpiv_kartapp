fcf8c083c25faed84a96f5cae01f9d14ec2d93f9
Martin Pettersson <kontakt@martinpettersson.se>
2019-06-25 09:11:35 +0200

    Fading list animation.

--- Groceries/platforms/android/app/src/main/assets/app/views/list/list.js
+++ Groceries/platforms/android/app/src/main/assets/app/views/list/list.js
@@ -13,11 +13,16 @@ var pageData = observableModule.fromObject({
 
 exports.loaded = function (args) {
     page = args.object;
+    var listView = page.getViewById('groceryList');
     page.bindingContext = pageData;
     groceryList.empty();
     pageData.set('isLoading', true);
     groceryList.load().then(function() {
         pageData.set("isLoading", false);
+        listView.animate({
+            opacity: 1,
+            duration: 1000
+        });
     });
 };
 
