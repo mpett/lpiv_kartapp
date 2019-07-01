0ce6f61c513f5a4d33c94a706854ddc6081fb61f
Martin Pettersson <kontakt@martinpettersson.se>
2019-07-01 13:09:39 +0200

    Working react-native-maps sample (AwesomeProject)

--- core/sort.js
+++ core/sort.js
@@ -1,22 +1,31 @@
-function sieve(n) {
-    var a = new Array(n);
-    for (var i = 2; i < n; i++) {
-        a[i] = true;
+function heapsort(a) {
+    var n = a.length;
+    for (var i = Math.floor(n / 2 - 1); i >= 0; i--) {
+        heapify(a, n, i);
     }
-    for (var i = 2; i < Math.sqrt(n); i++) {
-        for (var j = i * i; j < n; j += i) {
-            if (a[j]) {
-                a[j] = false;
-            }
-        }
+    for (var i = n - 1; i >= 0; i--) {
+        var tmp = a[i];
+        a[i] = a[0];
+        a[0] = tmp;
+        heapify(a, i, 0);
     }
-    var b = new Array();
-    for (var i = 0; i < n; i++) {
-        if (a[i]) {
-            b.push(i);
-        }
+}
+function heapify(a, n, i) {
+    var hi = i;
+    var l = 2 * i + 1;
+    var r = 2 * i + 2;
+    if (l < n && a[l] > a[hi]) {
+        hi = l;
     }
-    return b;
+    if (r < n && a[r] > a[hi]) {
+        hi = r;
+    }
+    if (hi != i) {
+        var tmp = a[hi];
+        a[hi] = a[i];
+        a[i] = tmp;
+        heapify(a, n, hi);
+    }
 }
 function quicksort(a, lo, hi) {
     if (lo < hi) {
@@ -41,14 +50,37 @@ function partition(a, lo, hi) {
     a[hi] = tmp;
     return i + 1;
 }
+function sieve(n) {
+    var a = new Array(n);
+    for (var i = 2; i < n; i++) {
+        a[i] = true;
+    }
+    for (var i = 2; i < Math.sqrt(n); i++) {
+        for (var j = i * i; j < n; j += i) {
+            if (a[j]) {
+                a[j] = false;
+            }
+        }
+    }
+    var b = new Array();
+    for (var i = 0; i < n; i++) {
+        if (a[i]) {
+            b.push(i);
+        }
+    }
+    return b;
+}
 function main() {
     console.log("Hello World");
-    var a = [651, 51, 7, 15, 51, 7, 1, 81, 9, 6, 1, 16, 51, -21, 16, 6];
+    var a = [2121, 5, 8, 8, 85, 85, 4, 984, 6, 654, -984, 554, 45, 8];
     console.log(a);
-    quicksort(a, 0, a.length - 1);
+    heapsort(a);
     console.log(a);
-    var b = sieve(117899);
+    var b = [6514, 1, 7, 219, 81, 8, 854, 96, 85, -6516, 652, 6, -5161, -651, 54];
     console.log(b);
-    console.log(b[b.length - 1]);
+    quicksort(b, 0, b.length - 1);
+    console.log(b);
+    var c = sieve(23425334);
+    console.log(c[c.length - 1]);
 }
 main();
--- core/sort.ts
+++ core/sort.ts
@@ -1,3 +1,67 @@
+function heapsort(a:number[]):void {
+    var n:number = a.length;
+
+    for (let i:number = Math.floor(n/2-1); i >= 0; i--) {
+        heapify(a, n, i);
+    }
+
+    for (let i:number = n - 1; i >= 0; i--) {
+        let tmp:number = a[i];
+        a[i] = a[0];
+        a[0] = tmp;
+        heapify(a, i, 0);
+    } 
+}
+
+function heapify(a:number[], n:number, i:number):void {
+    var hi:number = i;
+    var l:number = 2*i+1;
+    var r:number = 2*i+2;
+
+    if (l < n && a[l] > a[hi]) {
+        hi = l;
+    }
+
+    if (r < n && a[r] > a[hi]) {
+        hi = r;
+    }
+
+    if (hi != i) {
+        let tmp:number = a[hi];
+        a[hi] = a[i];
+        a[i] = tmp;
+        heapify(a, n, hi);
+    }
+}
+
+function quicksort(a:number[], lo:number, hi:number):void {
+    if (lo < hi) {
+        let p:number = partition(a, lo, hi);
+        quicksort(a, p+1, hi);
+        quicksort(a, lo, p-1);
+    }
+}
+
+function partition(a:number[], lo:number, hi:number):number {
+    var pivot:number = a[hi];
+    var i:number = lo - 1;
+
+    for (let j:number = lo; j < hi; j++) {
+        if (pivot > a[j]) {
+            i++;
+            let tmp:number = a[i];
+            a[i] = a[j];
+            a[j] = tmp;
+        }
+    }
+
+    let tmp:number = a[i+1];
+    a[i+1] = a[hi];
+    a[hi] = tmp;
+
+    return i+1;
+}
+
 function sieve(n:number):number[] {
     var a:boolean[] = new Array(n);
 
@@ -24,43 +88,18 @@ function sieve(n:number):number[] {
     return b;
 }
 
-function quicksort(a:number[], lo:number, hi:number):void {
-    if (lo < hi) {
-        let p:number = partition(a, lo, hi);
-        quicksort(a, p+1, hi);
-        quicksort(a, lo, p-1);
-    }
-}
-
-function partition(a:number[], lo:number, hi:number):number {
-    var pivot:number = a[hi];
-    var i:number = lo - 1;
-
-    for (let j:number = lo; j < hi; j++) {
-        if (pivot > a[j]) {
-            i++;
-            let tmp:number = a[i]
-            a[i] = a[j];
-            a[j] = tmp;
-        }
-    }
-
-    var tmp:number = a[i+1];
-    a[i+1] = a[hi];
-    a[hi] = tmp;
-
-    return i+1;
-}
-
 function main():void {
     console.log("Hello World");
-    var a:number[] = [651,51,7,15,51,7,1,81,9,6,1,16,51,-21,16,6];
+    var a:number[] = [2121,5,8,8,85,85,4,984,6,654,-984,554,45,8];
     console.log(a);
-    quicksort(a, 0, a.length - 1);
+    heapsort(a);
     console.log(a);
-    var b:number[] = sieve(117899);
+    var b:number[] = [6514,1,7,219,81,8,854,96,85,-6516,652,6,-5161,-651,54];
     console.log(b);
-    console.log(b[b.length - 1]);
+    quicksort(b, 0, b.length - 1);
+    console.log(b);
+    var c:number[] = sieve(23425334);
+    console.log(c[c.length-1]);
 }
 
 main();
