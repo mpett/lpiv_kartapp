ad87301625ae32be68681388abb48b05e756be11
Martin Pettersson <kontakt@martinpettersson.se>
2019-07-10 14:01:13 +0200

    FlatList minimal example

---
+++ core/FlatListBasics/App.js
@@ -0,0 +1,69 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Fragment, Component} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  FlatList
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const rows = [
+  {id: 0, text: 'View'},
+  {id: 1, text: 'Text'},
+  {id: 2, text: 'Image'},
+  {id: 3, text: 'ScrollView'},
+  {id: 4, text: 'ListView'},
+]
+
+const extractKey = ({ id }) => id.toString()
+
+export default class App extends Component {
+
+  renderItem = ({ item }) => {
+    return (
+      <Text style={styles.row}>
+        {item.text}
+      </Text>
+    )
+  }
+
+  render() {
+    return (
+      <FlatList 
+        style={styles.container}
+        data={rows}
+        renderItem={this.renderItem}
+        keyExtractor={extractKey}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  row: {
+    padding: 15,
+    marginBottom: 5,
+    backgroundColor: 'skyblue',
+  },
+})
