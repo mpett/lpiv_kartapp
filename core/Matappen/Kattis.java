3ca4048833505595e5341861072125f5bf88298c
Martin Pettersson <martin@tivala.se>
2019-10-18 12:51:07 +0200

    Kattis update

--- core/Matappen/Kattis.java
+++ core/Matappen/Kattis.java
@@ -5,6 +5,74 @@ import java.io.InputStreamReader;
 public class Kattis {
     private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
+    public static void main(String[] args) throws IOException {
+        apaxiaans();
+    }
+
+    private static void apaxiaans() throws IOException {
+        String input = reader.readLine();
+        int n = input.length();
+        StringBuilder sb = new StringBuilder(input);
+
+        for (int j = 0; j < n-1; j++) {
+            for (int i = 0; i < n - 1; i++) {
+                int tmp = i;
+                while (input.charAt(i) == input.charAt(i+1)) {
+                    sb.deleteCharAt(i);
+                    input = sb.toString();
+                    n = input.length();
+                    if ((i) >= (n-1))
+                        break;
+                }
+                i = tmp;
+            }
+        }
+
+        String output = sb.toString();
+        System.out.println(output);
+    }
+
+    private static void lastFactorialDigit() throws IOException {
+        int t = Integer.parseInt(reader.readLine());
+
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(reader.readLine());
+            long factorial = simpleFactorial(n);
+            String factorialString = "" + factorial;
+            System.out.println(factorialString.charAt(factorialString.length() - 1));
+        }
+    }
+
+    private static long simpleFactorial(int n) {
+        long fact = 1;
+
+        for (int i = 2; i <= n; i++) {
+            fact = fact * i;
+        }
+
+        return fact;
+    }
+
+    private static void avion() throws IOException {
+        int n = 5;
+        int result = 0;
+        String resultString = "";
+
+        for (int i = 0; i < n; i++) {
+            String registrationCode = reader.readLine();
+            if (registrationCode.contains("FBI")) {
+                result++;
+                resultString += (i+1) + " ";
+            }
+        }
+
+        if (result == 0) {
+            System.out.println("HE GOT AWAY!");
+        } else {
+            System.out.println(resultString);
+        }
+    }
+
     private static void tarifa() throws IOException {
         int x = Integer.parseInt(reader.readLine());
         int n = Integer.parseInt(reader.readLine());
@@ -52,9 +120,5 @@ public class Kattis {
         }
 
         System.out.println(result);
-    }
-
-    public static void main(String[] args) throws IOException {
-        tarifa();
     }
 }
